6a631b94c6a3a51170aa0536913a66b2b063a818
itsamayo <ashleymichaelsanders@gmail.com>
2025-01-13 16:06:02 -0800

    added conform for linting

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -30,7 +30,45 @@ return {
     },
     config = true,
     lazy = false
-  }
+  },
+  {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        javascript = { "eslint_d" },
+        typescript = { "eslint_d" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        svelte = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        graphql = { "prettier" },
+        lua = { "stylua" },
+        python = { "isort", "black" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500,
+      },
+    })
+
+    vim.keymap.set({ "n", "v" }, "<leader>fx", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500,
+      })
+    end, { desc = "Format file or range (in visual mode)" })
+  end,
+}
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
