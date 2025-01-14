dc981058d086078bb9c7b6223f0ebfd8eab9d387
itsamayo <ashleymichaelsanders@gmail.com>
2025-01-14 10:46:39 -0800

    add formatting

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -50,7 +50,7 @@ return {
         format_on_save = {
           lsp_fallback = true,
           async = false,
-          timeout_ms = 500,
+          timeout_ms = 1000,
         },
       }
 
@@ -58,9 +58,39 @@ return {
         conform.format {
           lsp_fallback = true,
           async = false,
-          timeout_ms = 500,
+          timeout_ms = 1000,
         }
       end, { desc = "Format file or range (in visual mode)" })
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    event = {
+      "BufReadPre",
+      "BufNewFile",
+    },
+    config = function()
+      local lint = require "lint"
+
+      lint.linters_by_ft = {
+        javascript = { "eslint_d" },
+        typescript = { "eslint_d" },
+        javascriptreact = { "eslint_d" },
+        typescriptreact = { "eslint_d" },
+      }
+
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+
+      vim.keymap.set("n", "<leader>fs", function()
+        lint.try_lint()
+      end, { desc = "Trigger linting for current file" })
     end,
   },
   -- {
