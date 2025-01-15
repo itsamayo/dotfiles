9b6e1f9d7ad5e77a23eaaef2582ffe2961a67b04
itsamayo <ashleymichaelsanders@gmail.com>
2025-01-15 15:32:00 -0800

    added mappings for telescopr

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,10 +8,10 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- Hop
-map("n", "<space>jw", "<cmd>HopWord<CR>", { desc = "Hop to word" })
-map("n", "<space>jp", "<cmd>HopPattern<CR>", { desc = "Hop pattern" })
-map("n", "<space>jc", "<cmd>HopChar1<CR>", { desc = "Hop to char1" })
-map("n", "<space>jl", "<cmd>HopLine<CR>", { desc = "Hop to line" })
+map("n", "<leader>jw", "<cmd>HopWord<CR>", { desc = "Hop to word" })
+map("n", "<leader>jp", "<cmd>HopPattern<CR>", { desc = "Hop pattern" })
+map("n", "<leader>jc", "<cmd>HopChar1<CR>", { desc = "Hop to char1" })
+map("n", "<leader>jl", "<cmd>HopLine<CR>", { desc = "Hop to line" })
 
 -- Trouble
 map("n", "<leader>qq", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Diagnostics (Trouble)" })
@@ -27,15 +27,27 @@ map("n", "<leader>ql", "<cmd>Trouble loclist toggle<cr>", { desc = "Location Lis
 map("n", "<leader>qf", "<cmd>Trouble qflist toggle<cr>", { desc = "Quickfix List (Trouble)" })
 
 -- Telescope
-map("n", "<space>fe", "<cmd>Telescope diagnostics<CR>", { desc = "See issues" })
-map("n", "<space>fr", "<cmd>Telescope oldfiles<CR>", { desc = "See old files" })
-map("n", "<space>jd", "<cmd>Telescope lsp_definitions<CR>", { desc = "Open lsp definiions" })
-map("n", "<space>jr", "<cmd>Telescope lsp_references<CR>", { desc = "Open lsp references" })
-map("n", "<space>jt", "<cmd>Telescope lsp_type_definitions<CR>", { desc = "Open lsp type definitions" })
-map("n", "<space>ji", "<cmd>Telescope lsp_implementations<CR>", { desc = "Open lsp implementations" })
-map("n", "<space>ga", "<cmd>Telescope git_branches<CR>", { desc = "See all branches" })
-map("n", "<space>gca", "<cmd>Telescope git_commits<CR>", { desc = "See all git commits" })
-map("n", "<space>gcb", "<cmd>Telescope git_bcommits<CR>", { desc = "See buffer commits" })
-map("n", "<space>gs", "<cmd>Telescope git_stash<CR>", { desc = "See git stash" })
+map("n", "<leader>fe", "<cmd>Telescope diagnostics<CR>", { desc = "See issues" })
+map("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "See old files" })
+map("n", "<leader>rd", "<cmd>Telescope lsp_definitions<CR>", { desc = "Open lsp definiions" })
+map("n", "<leader>rr", "<cmd>Telescope lsp_references<CR>", { desc = "Open lsp references" })
+map("n", "<leader>rt", "<cmd>Telescope lsp_type_definitions<CR>", { desc = "Open lsp type definitions" })
+map("n", "<leader>ri", "<cmd>Telescope lsp_implementations<CR>", { desc = "Open lsp implementations" })
+map(
+  "n",
+  "<leader>rs",
+  "<cmd> split | lua vim.lsp.buf.definition()<CR>",
+  { desc = "Open definition in horizontal split" }
+)
+map(
+  "n",
+  "<leader>rv",
+  "<cmd> vsplit | lua vim.lsp.buf.definition()<CR>",
+  { desc = "Open definition in vertical split" }
+)
+map("n", "<leader>ga", "<cmd>Telescope git_branches<CR>", { desc = "See all branches" })
+map("n", "<leader>gca", "<cmd>Telescope git_commits<CR>", { desc = "See all git commits" })
+map("n", "<leader>gcb", "<cmd>Telescope git_bcommits<CR>", { desc = "See buffer commits" })
+map("n", "<leader>gs", "<cmd>Telescope git_stash<CR>", { desc = "See git stash" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
