a8335a95f918bd4ca6be2b26db99762a1bbd6ac6
itsamayo <ashleymichaelsanders@gmail.com>
2025-01-16 11:49:32 -0800

    package update

--- lua/plugins/microscope.lua
+++ lua/plugins/microscope.lua
@@ -4,7 +4,7 @@ return {
     config = function()
       require("microscope.module").setup {
         keymaps = {
-          fold = "<leader>ff",
+          fold = "<leader>fg",
         },
       }
     end,
