88e716b0ff79b5eac8062a353f2321247accd75d
itsamayo <ashleymichaelsanders@gmail.com>
2025-01-24 12:52:44 -0800

    add showkeys

---
+++ lua/plugins/showkeys.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvzone/showkeys",
+    cmd = "ShowkeysToggle",
+    opts = {
+      timeout = 5,
+      maxkeys = 5,
+      position = "top-right",
+      -- more opts
+    },
+  },
+}
